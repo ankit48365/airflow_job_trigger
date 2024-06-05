e9a5a416603eef4f105e4ef1bacb0611837ef051
ankit K <ankit48365@gmail.com>
2024-06-04 23:01:35 -0400

    A

---
+++ example_2/example_2.py
@@ -0,0 +1,13 @@
+import os
+
+# run the below command on your terminal (if linux distribution) and then : source ~/.bashrc
+# echo 'export msg_local_variable="example2 - this message comes via local variable on system"' >> ~/.bashrc
+
+message= os.environ['msg_local_variable']
+
+# Print the value associated with the key 'message'
+print(message)
+
+
+
+
