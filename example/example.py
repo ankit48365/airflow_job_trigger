3484c774890a15ab5d59e5f6c65af4dd9444993c
ankit K <ankit48365@gmail.com>
2024-06-08 19:51:17 -0400

    A

--- example/example.py
+++ example/example.py
@@ -1,7 +1,6 @@
 
 ###########################################################################################
 # Example 1
-
 import yaml
 
 # Load the somefile.yml file
@@ -10,18 +9,12 @@ with open('somefile.yml', 'r') as file:
 
 # Reading key called message from the YAML 
 msg = config['message']
-
 # Print the value associated with the key 'message'
 print(msg)
 
-
 ###########################################################################################
 # Example 1
-
 import os
-
-# run the below command on your terminal (if linux distribution) and then : source ~/.bashrc
-# echo 'export msg_local_variable="example 2 - This message comes via local variable on system"' >> ~/.bashrc
 
 message= os.environ['msg_local_variable']
 # Print the value associated with the key 'message'
